95ea729d48d9228fd6c3f0c70f7037fc55a68867
rafaeel16 <rafaeel.16@gmail.com>
2017-11-03 21:46:50 -0200

    updating id_device when login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,18 +17,22 @@ class LoginController extends Controller
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
         
-            try {
-                // attempt to verify the credentials and create a token for the user
-                if (! $token = app('tymon.jwt.auth')->attempt($credentials)) {
-                    return response()->json(['error' => 'invalid_credentials'], 401);
-                }
-            } catch (JWTException $e) {
-                // something went wrong whilst attempting to encode the token
-                return response()->json(['error' => 'could_not_create_token'], 500);
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = app('tymon.jwt.auth')->attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
             }
-    
-            // all good so return the token
-            return response()->json(compact('token'));
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        
+        $usuario = Usuario::where('email', $credentials['email'])->first();
+        $usuario->update(['id_device' => $request->id_device]);
+        $usuario->save();
+
+        // all good so return the token
+        return response()->json(compact('token'));
     }
 
 }
